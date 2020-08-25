1f721c50c8d6ead2b0eb855164c2908934172397
Rick Kauffman <Rick@rickkauffman.com>
2020-08-25 15:19:22 -0700

    alpha

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # synergy-tweet
 Tweet things into a HPE Synergy OneView
+
+
+Set the twitter sensor to look for the word vlan in the tweet tweet_body
+Send vlanId to oneview in workflow
---
+++ actions/format_tweet.py
@@ -0,0 +1,44 @@
+# (C) Copyright 2019 Hewlett Packard Enterprise Development LP.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#  http://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# __author__ = "@netwookie"
+# __credits__ = ["Rick Kauffman"]
+# __license__ = "Apache2.0"
+# __maintainer__ = "Rick Kauffman"
+# __email__ = "rick.a.kauffman@hpe.com"
+
+# A python script for formatting twitter data
+
+import json
+import requests
+from st2common.runners.base_action import Action
+
+class FormatTweet(Action):
+
+    def run(self, tweet_body):        # Get trigger dictionaary.
+        if tweet_body['user']['screen_name'] == 'netwookie':
+            line = tweet_body['text']
+            # Anything between two slashes is the data we want
+            vlan_data={}
+            x,vlan,y = line.split('/')
+            # Build vlan dictionary for OneView
+            vlanId=vlan
+            name='Automated vlan via with Twitter & StackStorm'
+            purpose="General"
+            smartLink=False
+            privateNetwork=False
+            connectionTemplateUri=None
+            ethernetNetworkType='Tagged'
+            # type='ethernet-networkV4'
+        return (True, vlanId,name,purpose,smartLink,privateNetwork,connectionTemplateUri,ethernetNetworkType)
